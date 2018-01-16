c0c4b385aaf84470adac16e082f54d5a24b35de0
Pavel Karachev <karachevpavel@gmail.com>
2018-01-16 13:26:06 +0300

    добавляет дополнительное 2 задание

---
+++ 02-homework-15-01-2018/extra/extra1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Реализовать RLE-сжатие: AAAB -> A3B, BCCDDDAXXXX -> BC2D3AX4
+ * @param  {string} value
+ * @return {string}
+ */
+function rle(value) {
+}
+
+
+console.log(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'));
